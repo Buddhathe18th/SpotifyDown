4b6b4d3cec3d73f9bdc962b513dc35c6ed584401
Alex Zhu <120733125+Buddhathe18th@users.noreply.github.com>
2025-09-11 10:43:49 -0400

    Authentication process
    This should work, but my client id and secret aren't accessable rn

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ from spotipy.oauth2 import SpotifyOAuth
 
 scope = "user-library-read"
 
-auth_manager = SpotifyOAuth(scope=scope, show_dialog=True)
+auth_manager = SpotifyOAuth(scope=scope, show_dialog=True, open_browser=False)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
+print("CLICK:")
+print(f"\n{auth_manager.get_authorize_url()}")
+redirect_response = input("\nPaste FULL REDIRECT LINK: ").strip()
+
+
 try:
+    code = auth_manager.parse_response_code(redirect_response)
+    token = auth_manager.get_access_token(code)
+    
+    sp = spotipy.Spotify(auth_manager=auth_manager)
     results = sp.playlist("41TSE7Yh4ZYy0xueqVg8cx")
     
     if not results['items']:
